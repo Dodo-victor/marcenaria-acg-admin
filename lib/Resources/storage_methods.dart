efb1ef18c18fd22916243e682b2d423f6e8ea387
unknown <you@example.com>
2023-10-28 19:47:40 +0100

    Storage2

--- lib/Resources/storage_methods.dart
+++ lib/Resources/storage_methods.dart
@@ -1,12 +1,20 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  _saveProductImage() async {
+  _saveProductImage({required String childName, required Uint8List file, required String productId }) async {
 
+Reference ref = await _storage.ref().child(childName).child(productId);
+UploadTask uploadTask = ref.putData(file);
+TaskSnapshot snap = await uploadTask;
+String downloadUrl = await snap.ref.getDownloadURL();
 
-
+return downloadUrl;
 
   }
 
